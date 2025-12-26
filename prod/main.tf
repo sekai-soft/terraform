469dd29dbb504cc5fff98ccc03b7c297b2b8219a
KTachibanaM <whj19931115@gmail.com>
2025-12-27 00:53:45 +0800

    comment out for now

--- prod/main.tf
+++ prod/main.tf
@@ -1,15 +1,15 @@
-module "mastodon-s3" {
-  source = "../modules/s3-bucket"
+# module "mastodon-s3" {
+#   source = "../modules/s3-bucket"
 
-  project = "mastodon"
-  enable_cname = false
-  public_read = true
-}
+#   project = "mastodon"
+#   enable_cname = false
+#   public_read = true
+# }
 
-module "db-backups-s3" {
-  source = "../modules/s3-bucket"
+# module "db-backups-s3" {
+#   source = "../modules/s3-bucket"
 
-  project = "db-backups"
-  enable_cname = false
-  public_read = false
-}
+#   project = "db-backups"
+#   enable_cname = false
+#   public_read = false
+# }
